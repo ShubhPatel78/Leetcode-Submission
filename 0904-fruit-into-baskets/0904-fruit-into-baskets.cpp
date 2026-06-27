b3b02b4d16649c2af5127a4586fff815f7a7d113
ShubhPatel1478 <pinalkashyap03@gmail.com>
2026-06-27 23:37:14 +0530

    Time: 40 ms (68.42%), Space: 84.9 MB (33.68%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
         unordered_map<int,int>freq;
         while(r<n){
              freq[fruits[r]]++;
-             while(freq.size()>2){
+             if(freq.size()>2){
                 freq[fruits[l]]--;
                 if(freq[fruits[l]]==0){
                     freq.erase(fruits[l]);
