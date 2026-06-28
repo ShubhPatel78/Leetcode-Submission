1ec98fc2b2f10c702f4f24038322d8009e9178c5
ShubhPatel1478 <pinalkashyap03@gmail.com>
2026-06-28 12:07:11 +0530

    Time: 17 ms (19.85%) | Memory: 12.6 MB (54.77%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string s, int k) {
+        int n=s.size();
+        int maxi=0;
+        int l=0;
+        int r=0;
+        int maxfreq=0;
+        map<char,int>mpp;
+        while(r<n){
+            mpp[s[r]]++;
+            maxfreq=max(maxfreq,mpp[s[r]]);
+            while(r-l+1-maxfreq>k){
+                mpp[s[l]]--;
+                maxfreq=max(maxfreq,mpp[s[l]]);
+                l++;
+            }
+            maxi=max(maxi,r-l+1);
+            r++;
+        }
+        return maxi;
+    }
+};
